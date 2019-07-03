756070d55aeac8419508c6fd1ba7614840be2369
nfeuer <nickfeuer@gmail.com>
2019-07-03 17:06:23 -0400

    publish failed

--- Duck/credentials.h
+++ Duck/credentials.h
@@ -3,15 +3,15 @@
 #ifndef __CREDENTIALS_H__
 #define __CREDENTIALS_H__
 
-#define SSID        "MySpectrumWiFi6e-2G_EXT" // Type your SSID
+#define SSID        "MySpectrumWiFi6e-2G" // Type your SSID
 #define PASSWORD    "famousocean778" // Type your Password
 
 //#define MQTT_MAX_PACKET_SIZE 1000;
 
 #define ORG         "9c6nfo"                  // "quickstart" or use your organisation
-#define DEVICE_ID   "ltt"
+#define DEVICE_ID   "SolarCT"
 #define DEVICE_TYPE "PAPA"                // your device type or not used for "quickstart"
-#define TOKEN       "DI2pFVAkmo&?D36h9z"      // your device token or not used for "quickstart"#define SSID        "nick_owl" // Type your SSID
+#define TOKEN       "rA7tGJNrLC8UA3S50?"      // your device token or not used for "quickstart"
 
 char server[]           = ORG ".messaging.internetofthings.ibmcloud.com";
 char topic[]            = "iot-2/evt/sensorup/fmt/json";
